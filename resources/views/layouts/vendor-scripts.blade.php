71797d96c96856563e0838429052e2c672ae5c24
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-03-15 22:39:40 +0600

    Script Updated

--- resources/views/layouts/vendor-scripts.blade.php
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -3,6 +3,7 @@
 <script src="{{ asset('build/libs/node-waves/waves.min.js') }}"></script>
 <script src="{{ asset('build/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ asset('build/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+
 {{--<script src='https://cdn.jsdelivr.net/npm/toastify-js'></script>--}}
 {{--<script src="{{ asset('build/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>--}}
 {{--<script src="{{ asset('build/libs/flatpickr/flatpickr.min.js')}}"></script>--}}
